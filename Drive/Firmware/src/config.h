045f436baf40fe2814819c3a1daf08f690e0114d
RKinosaki <ryusei.20213@gmail.com>
2026-05-25 16:56:34 +0200

    Remapped to v2.1

--- Drive/Firmware/src/config.h
+++ Drive/Firmware/src/config.h
@@ -1,11 +1,33 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 #include <Arduino.h>
+#include <Adafruit_ADS1X15.h>
 
-//BACK LEFT
-#define I5 8
-#define I6 7
-#define EN4 9
+//ADC
+#define SDA2 7
+#define SCL2 6
+#define ADC_CH2 ADS1X15_REG_CONFIG_MUX_SINGLE_2
+
+//M1
+#define I3 20
+#define I4 18
+#define EN2 19
+
+//M2
+#define I1 23
+#define I2 21
+#define EN1 20
+
+//M3
+#define I5 1
+#define I6 5
+#define EN3 0
+
+//M4
+#define I7 4
+#define I8 2
+#define EN4 3
+
 
 
 #endif
--- Drive/Firmware/src/main.cpp
+++ Drive/Firmware/src/main.cpp
@@ -1,20 +1,41 @@
 #include <Arduino.h>
 #include <config.h>
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
+Adafruit_ADS1015 ads;
 
 
 void setup() {
+  delay(5000);
+
   pinMode(I5, OUTPUT);
   pinMode(I6, OUTPUT);
-  pinMode(EN4, OUTPUT);
+  pinMode(EN3, OUTPUT);
   Serial.begin(9600);
+
+  Wire.begin();
+  Wire.setClock(10000);
+  delay(100);
+  if (!ads.begin(0x48, &Wire)){
+    Serial.println("ADS not initialised");
+  }
+  ads.startADCReading(2, false);
 }
 
 void loop() {
+  if (ads.conversionComplete()){
+    int16_t results = ads.getLastConversionResults();
+    Serial.print("Voltage: ");
+    Serial.print(ads.computeVolts(results));
+    Serial.println(" V");
+    ads.startADCReading(2, false);
+  }
   Serial.println("Going Forwards...");
   digitalWrite(I5, LOW);
   digitalWrite(I6, HIGH);
   for(int i = 0; i<255; i+=32){
-    analogWrite(EN4, i);
+    analogWrite(EN3, i);
     Serial.println(i);
     delay(500);
   }
@@ -22,7 +43,7 @@ void loop() {
   digitalWrite(I5, HIGH);
   digitalWrite(I6, LOW);
   for(int i = 0; i<255; i+=32){
-    analogWrite(EN4, i);
+    analogWrite(EN3, i);
     Serial.println(i);
     delay(500);
   }
