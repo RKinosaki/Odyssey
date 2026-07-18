9f07715e84ee34df38b4ce03d95d5e8e28b0199c
RKinosaki <ryusei.kinosaki@gmail.com>
2026-07-18 15:23:11 +0200

    Firmware for v2.0, and hardware fixes change for v2.1

--- PowerDistribution/Firmware/src/config.h
+++ PowerDistribution/Firmware/src/config.h
@@ -4,17 +4,17 @@
 
 //FET PINS
 #define FET_MAIN 1
-#define FET_ALPHA 7
-#define FET_BETA 8
-#define FET_GAMMA 9
-#define FET_DELTA 13
+#define FET_ALPHA 0
+#define FET_BETA 4
+#define FET_GAMMA 5
+#define FET_DELTA 6
 
 //I2C
 #define SCL 11
 #define SDA 12
 
 //I2C Address
-#define ADS_ADDR 0x48
+#define ADS_ADDR 0x49
 #define INA_ADDR 0x40
 #define LNA_ADDR 0X48
 
@@ -23,5 +23,6 @@
 
 //ADC
 #define ADC_PD_RATIO 0.15151515
+#define ADC_PD_RATIO_2 0.662252
 
 #endif
--- PowerDistribution/Firmware/src/main.cpp
+++ PowerDistribution/Firmware/src/main.cpp
@@ -6,7 +6,7 @@
 
 
 int fet[5] = {FET_MAIN, FET_ALPHA, FET_BETA, FET_GAMMA, FET_DELTA};
-INA219_WE ina = INA219_WE(&Wire);
+// INA219_WE ina = INA219_WE(&Wire);
 static int sampleIndex = 0;
 
 float currentSamples[FILTER_SAMPLE_SIZE] = {};
@@ -20,57 +20,57 @@ float offsetCurrent = 0;
 void setup(){
   delay(2000);
   Serial.begin(115200);
-  Wire.begin(SDA, SCL);
+  // Wire.begin(SDA, SCL);
   for (int i = 0; i<sizeof(fet)/sizeof(fet[0]); i++){
     pinMode(fet[i], OUTPUT);
     digitalWrite(fet[i], HIGH);
     Serial.println("FET " + String(i) + " Opened");
   }
 
-  if(!ina.init()){
-    Serial.println("INA219 Initialisation Error!");
-    while(1){}
-  }
-  ina.setShuntSizeInOhms(0.01);
+  // if(!ina.init()){
+  //   Serial.println("INA219 Initialisation Error!");
+  //   while(1){}
+  // }
+  // ina.setShuntSizeInOhms(0.01);
 
-  delay(500);
-  if((ina.getBusVoltage_V()+ina.getShuntVoltage_mV()/1000)<0.05){
-    offsetCurrent = ina.getCurrent_mA();
-  }
+  // delay(500);
+  // if((ina.getBusVoltage_V()+ina.getShuntVoltage_mV()/1000)<0.05){
+  //   offsetCurrent = ina.getCurrent_mA();
+  // }
   
 }
 
-void calculateNewAverageCurrent(){
-  for (int i = 0; i < FILTER_SAMPLE_SIZE; i++){
-    Serial.print(String(currentSamples[i]));
-    if (i != FILTER_SAMPLE_SIZE-1){
+// void calculateNewAverageCurrent(){
+//   for (int i = 0; i < FILTER_SAMPLE_SIZE; i++){
+//     Serial.print(String(currentSamples[i]));
+//     if (i != FILTER_SAMPLE_SIZE-1){
       
-    Serial.print(", ");
-    }
-  }
-  Serial.println();
-  float sampleSum = currentAverage*FILTER_SAMPLE_SIZE;                //Finds Σn
-  float newSum = sampleSum - currentSamples[sampleIndex];             //Removes Oldest Sample from Σn
-  currentSamples[sampleIndex] = ina.getCurrent_mA() - offsetCurrent;  //measures current amd repleaces the oldest sample
-  newSum += currentSamples[sampleIndex];                              //Adds the new sample to Σn
-  currentAverage = newSum/FILTER_SAMPLE_SIZE;                         //Finds new average
-  sampleIndex = (sampleIndex+1) % FILTER_SAMPLE_SIZE;                 //Increments the index, wraps around at end
-}
+//     Serial.print(", ");
+//     }
+//   }
+//   Serial.println();
+//   float sampleSum = currentAverage*FILTER_SAMPLE_SIZE;                //Finds Σn
+//   float newSum = sampleSum - currentSamples[sampleIndex];             //Removes Oldest Sample from Σn
+//   currentSamples[sampleIndex] = ina.getCurrent_mA() - offsetCurrent;  //measures current amd repleaces the oldest sample
+//   newSum += currentSamples[sampleIndex];                              //Adds the new sample to Σn
+//   currentAverage = newSum/FILTER_SAMPLE_SIZE;                         //Finds new average
+//   sampleIndex = (sampleIndex+1) % FILTER_SAMPLE_SIZE;                 //Increments the index, wraps around at end
+// }
 
 void loop(){
-  calculateNewAverageCurrent();
-  if(currentAverage>0.13){
-    digitalWrite(FET_ALPHA, LOW);
-    Serial.println("Emergeny Stop!");
-  }
-  shuntVoltage = ina.getShuntVoltage_mV();
-  busVoltage = ina.getBusVoltage_V();
-  Serial.print("Current is: ");
-  Serial.print(String(currentAverage/1000));
-  Serial.println("A");
+  // calculateNewAverageCurrent();
+  // if(currentAverage>0.13){
+  //   digitalWrite(FET_ALPHA, LOW);
+  //   Serial.println("Emergeny Stop!");
+  // }
+  // shuntVoltage = ina.getShuntVoltage_mV();
+  // busVoltage = ina.getBusVoltage_V();
+  // Serial.print("Current is: ");
+  // Serial.print(String(currentAverage/1000));
+  // Serial.println("A");
 
-  Serial.print("Voltage is: ");
-  Serial.print(String(busVoltage + shuntVoltage/1000));
-  Serial.println("V");
-  delay(1000);
+  // Serial.print("Voltage is: ");
+  // Serial.print(String(busVoltage + shuntVoltage/1000));
+  // Serial.println("V");
+  // delay(1000);
 }
